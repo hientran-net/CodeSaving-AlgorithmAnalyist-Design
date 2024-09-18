f0ca52b3a411f53a7b81fb7705276a9a1d857b8b
HienTran <hientm0978@gmail.com>
2024-09-18 14:09:46 +0700

    Add files via upload

---
+++ checkPrimeNumber.cpp/check_Prime_Number/check_Prime_Number/check_Prime_Number.cpp
@@ -0,0 +1,31 @@
+﻿// example method check Prime number using model
+// input - process - output
+
+#include <iostream>
+
+// Bool method check if number is PrimeNumber -> return true | else -> return false
+bool isPrimeNumber(int number)
+{
+	if (number <= 1)
+		return false;
+
+	for (int i = 2; i <= sqrt(number); i++)
+	{
+		if (number % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int number;
+	std::cout << "Enter number: ";
+	std::cin >> number;
+
+	if (isPrimeNumber(number))
+		std::cout << number << " is Prime number\n";
+	else if (!isPrimeNumber(number))
+		std::cout << number << " isn't Prime number\n";
+	return 0;
+}
